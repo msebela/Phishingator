e64c53d697b351cfb25753024880f1a82aa7e56a
Martin Šebela <martin.sebela@cesnet.cz>
2022-10-21 23:55:58 +0200

    Cron changes

--- src/cron/base.php
+++ src/cron/base.php
@@ -1,4 +1,7 @@
 <?php
+  // Definice DOCUMENT_ROOT, který v CRONu (v rámci kterého je tento soubor spouštěn) neexistuje.
+  $_SERVER['DOCUMENT_ROOT'] = dirname(__FILE__);
+
   require $_SERVER['DOCUMENT_ROOT'] . '/../config.php';
   require $_SERVER['DOCUMENT_ROOT'] . '/../globalFunctions.php';
 
