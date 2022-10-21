2370055a0fbd0cfc33bc0f87bbf3389069a5e443
Martin Šebela <martin.sebela@cesnet.cz>
2022-10-21 23:25:47 +0200

    Dockerfile fix

--- docker/web/set-apache-envs.sh
+++ docker/web/set-apache-envs.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+touch /var/www/phishingator/logs/log.log
+chmod 777 /var/www/phishingator/logs/log.log
+
 export
 envsubst < /etc/apache2/sites-enabled/phishingator.conf > /etc/apache2/sites-enabled/000-default.conf
 rm /etc/apache2/sites-enabled/phishingator.conf
