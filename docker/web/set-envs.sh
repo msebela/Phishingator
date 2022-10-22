257adc2bf012c45813c9d28a148b1fb376ef00c7
Martin Šebela <martin.sebela@cesnet.cz>
2022-10-22 13:44:13 +0200

    Deployment changes

--- docker/web/set-envs.sh
+++ docker/web/set-envs.sh
@@ -6,6 +6,5 @@ touch $APACHE_DOCUMENT_ROOT/../logs/log.log
 chmod 777 $APACHE_DOCUMENT_ROOT/../logs/log.log
 chmod 777 $APACHE_DOCUMENT_ROOT/../templates/sites-config
 
-export
 envsubst < /etc/apache2/sites-enabled/phishingator.conf > /etc/apache2/sites-enabled/000-default.conf
 rm /etc/apache2/sites-enabled/phishingator.conf
