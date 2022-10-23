a5787301d0663c65bd47b2b09ed0fe7cedbfb314
Martin Šebela <martin.sebela@cesnet.cz>
2022-10-24 00:12:01 +0200

    Deployment scripts refactoring

--- docker/web/set-envs.sh
+++ docker/web/set-envs.sh
@@ -2,9 +2,5 @@
 
 printenv >> /etc/environment
 
-touch $APACHE_DOCUMENT_ROOT/../logs/log.log
-chmod 777 $APACHE_DOCUMENT_ROOT/../logs/log.log
-chmod 777 $APACHE_DOCUMENT_ROOT/../templates/sites-config
-
 envsubst < /etc/apache2/sites-enabled/phishingator.conf > /etc/apache2/sites-enabled/000-default.conf
 rm /etc/apache2/sites-enabled/phishingator.conf
