cc7ebe93f7ce88f1379b30fe2c0dc85a8984d782
Martin Šebela <martin.sebela@cesnet.cz>
2022-10-24 15:29:19 +0200

    Refactoring

--- docker/web/set-envs.sh
+++ docker/web/set-envs.sh
@@ -8,3 +8,6 @@ chown www-data $APACHE_DOCUMENT_ROOT/../templates/sites-config
 
 envsubst < /etc/apache2/sites-enabled/phishingator.conf > /etc/apache2/sites-enabled/000-default.conf
 rm /etc/apache2/sites-enabled/phishingator.conf
+
+envsubst < /etc/msmtprc.template > /etc/msmtprc
+rm /etc/msmtprc.template
