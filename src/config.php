15dbddcca4588846383da984b38fe04afbc265e7
Martin Šebela <martin.sebela@cesnet.cz>
2022-12-08 14:50:56 +0100

    Typo fix

--- src/config.php
+++ src/config.php
@@ -284,7 +284,7 @@
   define('CAMPAIGN_STATS_AGGREGATION', (getenv('CAMPAIGN_STATS_AGGREGATION')) ? getenv('CAMPAIGN_STATS_AGGREGATION') : 1);
 
   /** Rozmazání identit uživatelů ve statistikách kampaní. */
-  define('CAMPAIGN_STATS_BLUR_IDENTITIES', (getenv('CAMPAIGN_STATS_HIDE_IDENTITIES')) ? getenv('CAMPAIGN_STATS_HIDE_IDENTITIES') : true);
+  define('CAMPAIGN_STATS_BLUR_IDENTITIES', (getenv('CAMPAIGN_STATS_BLUR_IDENTITIES')) ? getenv('CAMPAIGN_STATS_BLUR_IDENTITIES') : true);
 
 
 
