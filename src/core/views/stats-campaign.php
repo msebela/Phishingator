3b3d07185c15334a04ceee47ea1f00488fa63984
Martin Šebela <martin.sebela@cesnet.cz>
2022-12-13 11:11:14 +0100

    Fix blinking Bootstrap tooltip

--- src/core/views/stats-campaign.php
+++ src/core/views/stats-campaign.php
@@ -192,16 +192,16 @@
 
                 <input type="hidden" name="<?= ACT_STATS_REPORT_PHISH ?>" value="<?= $data['id_captured_data'] ?>">
 
-                <div class="text-success<?php if ($data['reported'] == 0): ?> user-phishing-report<?php endif; ?>" data-toggle="tooltip" data-placement="top" data-html="true" data-original-title="Uživatel nahlásil pokus o&nbsp;cvičný phishing<br>(např. na helpdesk, správci IT, &hellip;)">
+                <div class="tooltip-icon text-purple<?php if ($data['reported'] == 0): ?> user-phishing-report<?php endif; ?>" data-toggle="tooltip" data-placement="top" data-html="true" data-original-title="Uživatel nahlásil pokus o&nbsp;cvičný phishing<br>(např. na helpdesk, správci IT, &hellip;)">
                   <button type="submit" class="border-0 bg-transparent" onclick="if (!confirm('Opravdu chcete u uživatele upravit nastavení o nahlášení cvičného phishingu?')) return false;">
-                    <span data-feather="message-circle" class="text-purple"></span>
+                    <span data-feather="message-circle"></span>
                   </button>
                 </div>
               </form>
             </td>
             <td>
               <?php if (isset($testPageData[$data['id_user']])): ?>
-              <div class="cursor-help text-success" data-toggle="tooltip" data-placement="top" data-html="true" data-original-title="Uživatel navštívil stránku o&nbsp;absolvování phishingu<br><?= $testPageData[$data['id_user']] ?>">
+              <div class="tooltip-icon text-success cursor-help" data-toggle="tooltip" data-placement="top" data-html="true" data-original-title="Uživatel navštívil vzdělávací stránku o&nbsp;absolvování phishingu<br><?= $testPageData[$data['id_user']] ?>">
                 <span data-feather="flag"></span>
               </div>
               <?php endif; ?>
