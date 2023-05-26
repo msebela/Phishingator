b272bfb6e396e3a5551650fbf6c24b6e3765d9e2
Martin Šebela <martin.sebela@cesnet.cz>
2023-05-26 13:45:44 +0200

    Fraudulent websites preview changes

--- src/core/views/preview-phishing-website.php
+++ src/core/views/preview-phishing-website.php
@@ -1,5 +1,5 @@
 <style>
-  .preview-info {font-family: Verdana, Geneva, sans-serif; position: absolute; top: 0; left: 0; right: 0; padding: 20px; text-align: left; color: #fff; background: #272b30;}
+  .preview-info {font-family: Verdana, Geneva, sans-serif; position: absolute; top: 0; left: 0; right: 0; padding: 20px; text-align: left; color: #fff; background: #272b30; z-index: 100;}
   .preview-info h1 {margin: 0 0 5px 0; font-size: 18pt;}
   .preview-info p {margin: 0; font-size: 10pt;}
   .preview-clear {height: 110px;}
