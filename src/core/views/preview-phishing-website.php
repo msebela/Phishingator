fa80ef407a3dd77bbe1d02585531a978ec03515b
Martin Šebela <martin.sebela@cesnet.cz>
2023-07-28 15:46:06 +0200

    Fraudulent website preview info improvement

--- src/core/views/preview-phishing-website.php
+++ src/core/views/preview-phishing-website.php
@@ -1,11 +1,23 @@
 <style>
-  .preview-info {font-family: Verdana, Geneva, sans-serif; position: absolute; top: 0; left: 0; right: 0; padding: 20px; text-align: left; color: #fff; background: #272b30; z-index: 100;}
-  .preview-info h1 {margin: 0 0 5px 0; line-height: 1.5; font-size: 18pt; font-weight: 800;}
-  .preview-info p {margin: 0; font-size: 10pt;}
-  .preview-clear {height: 110px;}
+   body {margin-top: 100px !important;}
+  .phishingator-preview-info {display: flex; justify-content: space-between; font-family: Verdana, Geneva, sans-serif; position: absolute; top: 0; left: 0; right: 0; padding: 20px; text-align: left; color: #fff; background: #272b30; z-index: 100;}
+  .phishingator-preview-info h1 {margin: 0 0 5px 0; line-height: 1.5; font-size: 18pt; font-weight: 800;}
+  .phishingator-preview-info p {margin: 0; font-size: 10pt;}
+  .phishingator-preview-info a:link {text-decoration: none; color: #fff;}
+  .phishingator-preview-info a:hover {text-decoration: none; color: #aeb7bd;}
+  .phishingator-close-btn {width: 60px; display: inline-block; text-align: center; font-size: 9pt; line-height: 1.3}
+  .phishingator-close-btn span {font-size: 26pt; font-weight: 900;}
 </style>
-<div class="preview-info">
-  <h1>Phishingator · Náhled podvodné stránky</h1>
-  <p>Odkaz na náhled je unikátní pro přihlášeného uživatele a&nbsp;jeho platnost je časově omezena.</p>
+
+<div class="phishingator-preview-info">
+  <div>
+    <h1>Phishingator · Náhled podvodné stránky</h1>
+    <p>Odkaz na náhled je unikátní pro přihlášeného uživatele a&nbsp;jeho platnost je časově omezena.</p>
+  </div>
+  <div>
+    <a href="#" onclick="window.close()" class="phishingator-close-btn">
+      <span>&times;</span>
+      Zavřít
+    </a>
+  </div>
 </div>
-<div class="preview-clear"></div>
