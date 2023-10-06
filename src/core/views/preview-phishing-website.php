cda6a2176fd8c52d8c783188c90ccdeb0f7c134d
Martin Šebela <martin.sebela@cesnet.cz>
2023-10-06 20:52:34 +0200

    Minor changes in preview of fraudulent website template

--- src/core/views/preview-phishing-website.php
+++ src/core/views/preview-phishing-website.php
@@ -1,11 +1,11 @@
 <style>
-   body {margin-top: 100px !important;}
+   body {padding-top: 100px !important;}
   .phishingator-preview-info {display: flex; justify-content: space-between; font-family: Verdana, Geneva, sans-serif; position: absolute; top: 0; left: 0; right: 0; padding: 20px; text-align: left; color: #fff; background: #272b30; z-index: 100;}
-  .phishingator-preview-info h1 {margin: 0 0 5px 0; line-height: 1.5; font-size: 18pt; font-weight: 800;}
+  .phishingator-preview-info h1 {margin: 0 0 5px 0; line-height: 1.5; font-family: Verdana, Geneva, sans-serif; font-size: 18pt; font-weight: 800; color: #fff;}
   .phishingator-preview-info p {margin: 0; font-size: 10pt;}
   .phishingator-preview-info a:link {text-decoration: none; color: #fff;}
   .phishingator-preview-info a:hover {text-decoration: none; color: #aeb7bd;}
-  .phishingator-close-btn {width: 60px; display: inline-block; text-align: center; font-size: 9pt; line-height: 1.3}
+  .phishingator-close-btn {width: 60px; display: inline-block; font-family: Verdana, Geneva, sans-serif; text-align: center; font-size: 9pt; line-height: 1.3}
   .phishingator-close-btn span {font-size: 26pt; font-weight: 900;}
 </style>
 
