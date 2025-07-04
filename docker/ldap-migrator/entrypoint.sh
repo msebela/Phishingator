604bc2a399f51a1911b820b52eece652c02285ba
Martin Šebela <martin.sebela@cesnet.cz>
2025-07-04 12:29:51 +0200

    Improvements in services deployment

---
+++ docker/ldap-migrator/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+
+# TODO: Process the CSV containing the list of users into an LDIF file here (e.g. using a Python script), or run ldapsearch to export LDAP data to local.ldif
+
+mv local.ldif data/users.ldif
