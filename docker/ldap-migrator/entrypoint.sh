570322aef967f0497ec5c8a3054237ed3a71ef3a
Martin Šebela <martin.sebela@cesnet.cz>
2025-07-07 10:28:35 +0200

    Minor change in container volume mount path in LDAP service

--- docker/ldap-migrator/entrypoint.sh
+++ docker/ldap-migrator/entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 
 # TODO: Process the CSV containing the list of users into an LDIF file here (e.g. using a Python script), or run ldapsearch to export LDAP data to local.ldif
 
-mv local.ldif data/users.ldif
+mv local.ldif ldap/users.ldif
